a0213fa2385e439ef943c334f60d19db88ac6cbb
Kenji Wada <ch3cooh393@gmail.com>
2023-08-15 09:58:33 +0900

    update

--- TodoNote/TodoNote/UseCase/Common/CheckNetworkAccess/CheckNetworkAccessUseCaseResult.swift
+++ TodoNote/TodoNote/UseCase/Common/CheckNetworkAccess/CheckNetworkAccessUseCaseResult.swift
@@ -1,5 +1,5 @@
 //
-//  CheckVersionUseCaseResult.swift
+//  CheckNetworkAccessUseCaseResult.swift
 //
 //  Created by KENJIWADA on 2023/01/03.
 //  Copyright © 2023 KENJI WADA. All rights reserved.
