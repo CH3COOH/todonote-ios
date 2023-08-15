c121e7ec26d815beb2f848f8c8980fc5f5f1c93e
Kenji Wada <ch3cooh393@gmail.com>
2023-08-15 11:01:18 +0900

    ライブラリのアップデート (2023/08/15) (#36)

--- TodoNote/TodoNote/Screen/Common/BaseViewModel.swift
+++ TodoNote/TodoNote/Screen/Common/BaseViewModel.swift
@@ -13,7 +13,7 @@ class BaseViewModel: ObservableObject {
     @Published var alertItem: AlertItem?
 
     @MainActor
-    internal func show(error: Error, dismissAction: (() -> Void)? = nil) {
+    func show(error: Error, dismissAction: (() -> Void)? = nil) {
         alertItem = AlertItem(
             alert: Alert(
                 title: R.string.localizable.error.text,
